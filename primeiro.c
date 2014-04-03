eb37214018fe4d95cf376739429b71c0acea42bd
Clovis Fortuna <c.r.fortuna@hotmail.com>
2014-04-02 21:39:00 -0300

    Lista 1
    Questões de 1 a 4

---
+++ primeiro.c
@@ -0,0 +1,13 @@
+/*
+hello word
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("\nHello Word\n");
+	return 0;
+}
+
---
+++ quarto.c
@@ -0,0 +1,17 @@
+/*
+Escreva um programa que leia um caracter digitado pelo usuário, imprima o caracter digitado e o código ASCII correspondente a este caracter. 
+ */
+
+
+#include <stdio.h>
+
+int main()
+{
+char x;
+scanf("%c",&x);
+printf("\n%d",x);
+
+	
+	return 0;
+}
+
